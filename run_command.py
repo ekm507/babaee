25bf0c76e632c53187e0c090ba82ae9c3cec7f1d
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-18 15:55:49 +0430

    some exceptions

--- run_command.py
+++ run_command.py
@@ -20,6 +20,25 @@ def run_command(command, updates):
             "parse_mode":"MarkdownV2",
         }
 
+
+    except PermissionError:
+        message = {
+            "chat_id":chat_id,
+            "text":'permissionError',
+        }
+
+    except FileNotFoundError:
+        message = {
+            "chat_id":chat_id,
+            "text":'FileNotFound',
+        }
+
+    except subprocess.CalledProcessError as error_text:
+        message = {
+            "chat_id":chat_id,
+            "text":f'ERROR: {error_text}',
+        }
+
         requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage', data=message)
     except:
         pass
