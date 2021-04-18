485f804764270958b5e5b0a6493f150c1ec3a377
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-18 15:37:51 +0430

    first things

---
+++ babaee.py
@@ -0,0 +1,12 @@
+from config import bot_token, forward_chat_id, admins_chat_id_list
+
+import requests
+from pprint import pprint
+from time import sleep
+
+
+sleep_time = 0.1
+
+message_limit = 1
+message_offset = 0
+
