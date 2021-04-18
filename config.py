149b68c7d0ea3fa1e9daaba8dd2076585b1a82a5
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-18 15:36:07 +0430

    add conf file

---
+++ config.py
@@ -0,0 +1,10 @@
+# paste bot token here
+bot_token = "" # "something:something"
+
+# chat id to forward all messages to
+forward_chat_id = "" #"something"
+
+# chat id of admins
+admins_chat_id_list = [
+     # integers
+]
