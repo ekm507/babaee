4020b4bf2224451ee2b599479ad726d0b6bf3c60
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-18 15:38:49 +0430

    get messages

--- babaee.py
+++ babaee.py
@@ -10,3 +10,15 @@ sleep_time = 0.1
 message_limit = 1
 message_offset = 0
 
+
+while(True):
+
+    sleep(sleep_time)
+
+    try:
+
+        updates = requests.post(f'https://api.telegram.org/bot{bot_token}/getupdates?offset={message_offset}&limit={message_limit}')
+
+
+    except requests.exceptions.ConnectionError:
+        print('connection error')
