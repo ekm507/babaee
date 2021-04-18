13f01f4770dec0f57831f9efb1e67ee681e71cd5
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-18 15:40:10 +0430

    get first message text

--- babaee.py
+++ babaee.py
@@ -19,6 +19,15 @@ while(True):
 
         updates = requests.post(f'https://api.telegram.org/bot{bot_token}/getupdates?offset={message_offset}&limit={message_limit}')
 
+        if len(updates.json()['result']) < 1:
+            continue
+        for message in updates.json()['result']:
+            pprint(message)
+
+        if 'message' in updates.json()['result'][0].keys():
+            chat_id = updates.json()['result'][0]['message']['chat']['id']
+            text = updates.json()['result'][0]['message']['text']
+
 
     except requests.exceptions.ConnectionError:
         print('connection error')
