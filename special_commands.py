6bf4f807a751b3ed050adf156a77753a5fb9f442
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-19 22:38:17 +0430

    docs

--- special_commands.py
+++ special_commands.py
@@ -1,9 +1,12 @@
 import subprocess
 
+# run command using sh shell
 def __run_sh__(command):
+    # run command using sh shell
     return subprocess.check_output(command, shell=True)
 
 
+# a dict of special commands mapped to corresponding function
 special_commands = {
     '/sh':__run_sh__,
 }
