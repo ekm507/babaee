2b9fddf800c758cbf80ee22030bcd087712c129f
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-21 01:47:53 +0430

    add special command for file editing

--- special_commands.py
+++ special_commands.py
@@ -5,9 +5,22 @@ def __run_sh__(command):
     # run command using sh shell
     return subprocess.check_output(command, shell=True)
 
+def __edit_file__(command : str):
+    fileName = command.split(' ')[0].split('\n')[0]
+    text = command[len(fileName)+1:]
+    if len(text) < 1:
+        pass
+    try:
+        with open(fileName, 'w') as fileToEdit:
+            fileToEdit.write(text)
+        return 'File edited successfully'
+    except:
+        return 'Error: there was an error editing file'
+
 
 # a dict of special commands mapped to corresponding function
 special_commands = {
     '/sh':__run_sh__,
+    '/edit':__edit_file__
 }
 
