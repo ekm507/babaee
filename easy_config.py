e0a10c08fe8b7d58afffef3b546e9e24bb1454c9
Erfan Kheyrollahi <ekm507@gmail.com>
2023-03-07 20:27:17 +0330

    generate a password

--- easy_config.py
+++ easy_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import requests
+import random
 
 # read default config file
 config_text = open('config.py.default').read()
@@ -13,6 +14,8 @@ bot_token = input("paste bot token here:")
 config_text = config_text.replace('bot_token = ""', f'bot_token = "{bot_token}"')
 
 
+password = str(random.randrange(1000, 9999))
+print(f"now open chat of robot and send it this text: {password}")
 admins_chat_id = 1234
 
 config_text = config_text.replace('admins_chat_id_list = [', f'admins_chat_id_list = [\n{admins_chat_id},')
